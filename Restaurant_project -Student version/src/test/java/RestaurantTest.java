3a2ad2a760a7efa40434cbf5398f489184fda9ad
abhilashpillai21 <abhilashpillai21@gmail.com>
2021-05-05 23:46:01 +0530

    Update RestaurantTest.java
    Added two test methods : 
    when_item_is_removed_it_should_deduct_from_sum_of_price() 
    when_item_is_selected_it_should_add_to_sum_of_price()

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -1,5 +1,3 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -37,7 +35,7 @@ class RestaurantTest {
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
-        addTestRestaurant();
+        addToMenu();
 
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.addToMenu("Sizzling brownie",319);
@@ -45,7 +43,7 @@ class RestaurantTest {
     }
     @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
-        addTestRestaurant();
+        addToMenu();
 
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
@@ -54,13 +52,34 @@ class RestaurantTest {
 
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
-        addTestRestaurant();
+        addToMenu();
 
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
 
-    private void addTestRestaurant() {
+    @Test
+    public void when_item_is_selected_it_should_add_to_sum_of_price() {
+        addToMenu();
+        int priceBefore = restaurant.getTotalPrice(restaurant.getMenu());
+        restaurant.addToMenu("Sizzling brownie",319);
+        int priceAfter = restaurant.getTotalPrice(restaurant.getMenu());
+
+        assertEquals(priceAfter, priceBefore +  restaurant.getPrice("Sizzling brownie"));
+    }
+
+    @Test
+    public void when_item_is_removed_it_should_deduct_from_sum_of_price() throws itemNotFoundException {
+        addToMenu();
+        int priceBefore = restaurant.getTotalPrice(restaurant.getMenu());
+        int priceOfVL = restaurant.getPrice("Vegetable lasagne");
+        restaurant.removeFromMenu("Vegetable lasagne");
+        int priceAfter = restaurant.getTotalPrice(restaurant.getMenu());
+
+        assertEquals(priceAfter, priceBefore -  priceOfVL);
+    }
+
+    private void addToMenu() {
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
         restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
